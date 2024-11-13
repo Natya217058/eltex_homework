7b0f52c914f73708383e84ad0466469ac3aa0a09
anastasia1995tozikova <anastasia1995tozikova@gmail.com>
2024-11-13 21:37:08 +0700

    ukaz

--- dzc.cc
+++ dzc.cc
@@ -2,16 +2,21 @@
 #include <math.h>
 
 #define BASE 2
- int main (){
+ int main(){
 	 int N;
-	 printf ("Vvedite naturalnoe chislo N:");
-	 scanf ("%d",&N);
-	 if (N<1){
+	 int res = 1;
+	 int *pRes = &res;
+         printf ("Vvedite naturalnoe chislo N:");
+	 if (scanf("%d",&N) !=1 || N<1)
+	 {
 		 printf ("Oshibka");
 		 return 1;
 	 }
-	 int result = (int)pow(BASE,N);
-	 printf ("2^ %d=%d\n", N, result);
+for (int i=0; i<N; i++)
+{
+         *pRes *= BASE;	
+}
+	 printf ( "%d=%d\n", N, *pRes);
 	 return 0;
 
  }
